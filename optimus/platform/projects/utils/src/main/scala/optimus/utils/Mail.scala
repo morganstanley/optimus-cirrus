3315d6394f580b4fa8cda8ee922531395dede8d8
Optimus Cirrus Developers <optimus_cirrus@morganstanley.com>
2022-01-25 10:47:47 -0500

    OPTIMUS-36426 export changes from 378372e0226aab0a773550ade48e62119fb614a7
    original_id:1da0b80cf67e1cec4aedff816a1b8382fedb92fc

---
+++ optimus/platform/projects/utils/src/main/scala/optimus/utils/Mail.scala
@@ -0,0 +1,63 @@
+/*
+ * Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License").
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package optimus.utils
+
+import java.io.File
+import java.util.Properties
+
+import javax.activation.DataHandler
+import javax.activation.FileDataSource
+import javax.mail.Address
+import javax.mail.Message
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeBodyPart
+import javax.mail.internet.MimeMessage
+import javax.mail.internet.MimeMultipart
+
+object Mail {
+
+  def send(
+      from: String,
+      to: Seq[String],
+      subject: String,
+      body: String,
+      replyTo: Option[Seq[String]] = None,
+      mimeType: String = "text/html",
+      attachments: Seq[File] = Seq())(implicit properties: Properties = System.getProperties): Unit = {
+
+    val session = Session.getInstance(properties)
+    val multipart = new MimeMultipart()
+    val message = new MimeMessage(session)
+
+    message.setFrom(new InternetAddress(from))
+    message.addRecipients(Message.RecipientType.TO, to.map(new InternetAddress(_)).toArray[Address])
+    replyTo.foreach(replyTo => message.setReplyTo(replyTo.map(new InternetAddress(_)).toArray))
+
+    attachments.foreach { attachment =>
+      val messageBodyPart = new MimeBodyPart()
+      messageBodyPart.setDataHandler(new DataHandler(new FileDataSource(attachment)))
+      messageBodyPart.setFileName(attachment.getName)
+      multipart.addBodyPart(messageBodyPart)
+    }
+
+    val messageBodyPart = new MimeBodyPart()
+    messageBodyPart.setContent(body, mimeType)
+    multipart.addBodyPart(messageBodyPart)
+
+    message.setSubject(subject)
+    message.setContent(multipart)
+
+    Transport.send(message)
+  }
+}
