756d6897b740890b0a8f52ae241cd51ce114e3f1
Optimus Cirrus Developers <optimus_cirrus@morganstanley.com>
2022-02-18 08:09:13 -0500

    OPTIMUS-36426 export changes from ec44aeb4290a75635485a3fa95369a4396837bfa

--- optimus/platform/projects/utils/src/main/scala/optimus/utils/CollectionUtils.scala
+++ optimus/platform/projects/utils/src/main/scala/optimus/utils/CollectionUtils.scala
@@ -21,7 +21,10 @@ import scala.util._
 
 object CollectionUtils extends CollectionUtils {
   final class TraversableOps[A](val as: Traversable[A]) extends AnyVal {
-    //return 0 if empty, 1 if size 1, or 2 if size > 1
+
+    /**
+     * return 0 if empty, 1 if size 1, or 2 if size > 1
+     */
     private[this] def zeroOneOrMany: Int = as match {
       case x: SeqLike[_, _] @unchecked =>
         val lc = x.lengthCompare(1)
@@ -37,6 +40,12 @@ object CollectionUtils extends CollectionUtils {
         }
     }
 
+    /**
+     * This returns an object of type A
+     * if this is the only element of the Traversable.
+     * Otherwise an exception is thorwn.
+     *
+     */
     def single: A = zeroOneOrMany match {
       case 0 => throw new NoSuchElementException("single on empty Traversable")
       case 1 => as.head
@@ -53,6 +62,83 @@ object CollectionUtils extends CollectionUtils {
     def singleOrNone: Option[A] = zeroOneOrMany match {
       case 1 => Some(as.head)
       case _ => None
+    }
+
+    /**
+     * An element of type A is returned if the Traversable is a singleton.
+     * Otherwise an exception flies.
+     *
+     * @param f a function that composes an exception message according to type A.
+     * @return
+     */
+    def singleOrThrow(f: Traversable[A] => String): A =
+      if (zeroOneOrMany == 1) as.head
+      else throw new IllegalArgumentException(f(as))
+
+    /** Returns Some(x) if there is a single element or None if empty. If more than 1 elements are found, use fallback */
+    def singleOptionOr(fallback: Option[A]): Option[A] =
+      if (zeroOneOrMany < 2) as.headOption else fallback
+
+    /**
+     * Returns true if the Traversable contains one element ( possibly multiple times ).
+     * Returns false otherwise.
+     */
+    def isSingleDistinct: Boolean = zeroOneOrMany match {
+      case 0 => false
+      case 1 => true
+      case _ => as.toSet.size == 1
+    }
+
+    /**
+     * Returns an element of type A if this is the only member of the Traversable.
+     * Otherwise an exception is thrown.
+     */
+    def singleDistinct: A = zeroOneOrMany match {
+      case 0 => throw new IllegalArgumentException("Expected single element, but was empty!")
+      case 1 => as.head
+      case _ =>
+        as.toSet.size match {
+          case 1 => as.head
+          case n =>
+            throw new IllegalArgumentException(
+              s"Expected single distinct element, but found $n: [${as.mkString(",")}]")
+        }
+    }
+
+    /**
+     * Returns None if the Traversable is empty.
+     * Returns Some(a) if a is the only element of type A contained by the Traversable.
+     * Otherwise exception flies.
+     * @return
+     */
+    def singleDistinctOption: Option[A] =
+      if (zeroOneOrMany < 2) as.headOption
+      else
+        as.toSet.size match {
+          case 0 | 1 => as.headOption
+          case n =>
+            throw new IllegalArgumentException(
+              s"Expected zero or one distinct element, but found $n: [${as.mkString(",")}]")
+        }
+
+
+    /** Not async friendly, for that see singleOrElseAsync. */
+    def singleOptionOrElse(nonSingleHandler: (Int, Traversable[A]) => Option[A]): Option[A] = as.size match {
+      case 1 => as.headOption
+      case n => nonSingleHandler(n, as)
+    }
+
+
+    /** Not async friendly, for that see singleOrElseAsync. */
+    def singleOr(nonSingleHandler: => A): A = as.size match {
+      case 1 => as.head
+      case n => nonSingleHandler
+    }
+
+    /** Not async friendly, for that see singleOrElseAsync. */
+    def singleOrElse(nonSingleHandler: (Int, Traversable[A]) => A): A = as.size match {
+      case 1 => as.head
+      case n => nonSingleHandler(n, as)
     }
   }
 }
@@ -85,7 +171,9 @@ trait CollectionUtils {
   }
   implicit class OptimusOptionOps[A](underlying: Option[A]) {
     def getOrThrow(msg: => String): A = underlying.getOrElse(throw new IllegalArgumentException(msg))
-    def onEmpty(action: => Unit): Option[A] = { if (underlying.isEmpty) action else (); underlying }
+    def onEmpty(action: => Unit): Option[A] = {
+      if (underlying.isEmpty) action else (); underlying
+    }
   }
 
   implicit class OptimusTryOps[A](underlying: Try[A]) {
@@ -95,7 +183,8 @@ trait CollectionUtils {
     }
     def onEmpty(action: Throwable => Unit): Try[A] = onFailure(action)
     def onFailure(action: Throwable => Unit): Try[A] = {
-      if (underlying.isFailure) action(underlying.failed.get) else (); underlying
+      if (underlying.isFailure) action(underlying.failed.get) else ();
+      underlying
     }
   }
 }
